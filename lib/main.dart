982d5da6847ed270f1d89ce27e5f14b99c487fe9
hadleycode <hadleydaniel@gmail.com>
2019-08-21 15:27:55 +0200

    add factory. Functioning hero

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:test_hero/screens/card_screen.dart';
-import 'package:test_hero/screens/home_screen.dart';
 import 'package:test_hero/screens/select_screen.dart';
 
 Future main() async {
@@ -13,15 +12,39 @@ class SelectCardApp extends StatefulWidget {
 }
 
 class _SelectCardAppState extends State<SelectCardApp> {
+  final navigatorKey = GlobalKey<NavigatorState>();
+
+  final screenRouteFactories = {
+    '/': () => MaterialPageRoute(builder: (context) => CardScreen()),
+    "Select": () => MaterialPageRoute(builder: (context) => SelectScreen()),
+  };
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: HomeScreen.id,
-      routes: {
-        HomeScreen.id: (context) => HomeScreen(),
-        CardScreen.id: (context) => CardScreen(),
-        SelectScreen.id: (context) => SelectScreen(),
-      },
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          body: _buildBody(),
+          bottomNavigationBar: _buildBottomNavigationBar(context),
+        ),
+      );
+
+  Widget _buildBody() => MaterialApp(
+      navigatorKey: navigatorKey,
+      onGenerateRoute: (route) => screenRouteFactories[route.name]());
+
+  Widget _buildBottomNavigationBar(context) => BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          _buildBottomNavigationBarItem(
+              "Card", Icons.text_fields, Colors.lightBlue),
+          _buildBottomNavigationBarItem(
+              "Select", Icons.settings, Colors.blueAccent),
+        ],
+        onTap: (routeIndex) => navigatorKey.currentState
+            .pushNamed(screenRouteFactories.keys.toList()[routeIndex]),
+      );
+
+  _buildBottomNavigationBarItem(name, icon, colour) => BottomNavigationBarItem(
+      icon: Icon(icon), title: Text(name), backgroundColor: colour);
 }
